c12bac83cc0c2b0a7363ec0b31c94a8fafe082db
Aidan Heller <asfheller@gmail.com>
2017-10-20 22:56:29 -0700

    Finish styling login form for now

--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -4,22 +4,22 @@ import "./LoginForm.css"
 const LoginForm = (props) => {
     return (
         <div id="loginForm">
-            <form onSubmit={props.onSubmit}>
-                <div className="header">
-                    <h1>Login</h1>
-                </div>
-                <div className="usernameContainer">
-                    <label>Username</label>
-                    <input type="text" value={props.username} onChange={props.onUsernameChange}/>
-                </div>
-                <div className="passwordContainer">
-                    <label>Password</label>
-                    <input type="password" value={props.password} onChange={props.onPasswordChange}/>
-                </div>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
+            <div id="container">
+                <form onSubmit={props.onSubmit}>
+                    <div className="content">
+                        <h1>Login</h1>
+                    </div>
+                    <div className="content">
+                        <input type="text" placeholder="Username" value={props.username} onChange={props.onUsernameChange}/>
+                    </div>
+                    <div className="content">
+                        <input type="password" placeholder="Password" value={props.password} onChange={props.onPasswordChange}/>
+                    </div>
+                    <div className="content">
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
