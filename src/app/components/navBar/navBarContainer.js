ed9c069d1548dc09a61a577197ddf23c2ac52d7c
Aidan Heller <asfheller@gmail.com>
2017-10-21 09:18:20 -0700

    Add navbar

---
+++ src/app/components/navBar/navBarContainer.js
@@ -0,0 +1,12 @@
+import React, { Component} from 'react';
+import NavBar from "./NavBar";
+
+class NavBarContainer extends Component {
+
+    render() {
+        return <NavBar/>;
+    }
+    
+}
+
+export default NavBarContainer;
