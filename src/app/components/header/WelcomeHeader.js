5914594aa74b1e1af4b8950f7c005c34560d1843
Aidan Heller <asfheller@gmail.com>
2017-10-24 09:05:15 -0700

    Add header to welcome page

--- src/app/components/header/WelcomeHeader.js
+++ src/app/components/header/WelcomeHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Header.css'
+import './WelcomeHeader.css'
 
 const Header = (props) => {
     return (
--- src/app/pages/Welcome.js
+++ src/app/pages/Welcome.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import NavLink from "../components/navBar/NavLink";
 import NavBar from "../components/navBar/NavBar";
+import WelcomeHeader from "../components/header/WelcomeHeader";
 
 const Welcome = () => {
     return (
         <div id="welcomePage">
             <NavBar>
-                <NavLink className="navLink" to="/">Sign Up</NavLink>
-                <NavLink className="navLink" to="/">Login</NavLink>
+                <NavLink to="/">Sign Up</NavLink>
+                <NavLink to="/">Login</NavLink>
             </NavBar>
+            <WelcomeHeader/>
         </div>
     );
 };
