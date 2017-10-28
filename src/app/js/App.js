93e6b00fb9d4ac9be249382d5763cdbdbbeb3bd2
Aidan Heller <asfheller@gmail.com>
2017-10-28 15:39:23 -0700

    Kill login page

--- src/app/js/App.js
+++ src/app/js/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import Welcome from './pages/Welcome';
 import '../scss/App.css';
-import LoginPage from "./pages/LoginPage";
 
 const App = () => {
     return (
         <Switch>
             <Route exact path="/" component={Welcome}/>
-            <Route path="/login" component={LoginPage}/>
         </Switch>
     )
 };
