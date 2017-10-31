a899b2dfa558a2a27e5924af5c6959d085d9a406
Aidan Heller <asfheller@gmail.com>
2017-10-31 09:48:06 -0700

    Delete poorly structured login form

--- src/app/js/pages/WelcomePage.js
+++ src/app/js/pages/WelcomePage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import WelcomeHeader from "../components/WelcomeHeader";
 import NavBar from "../components/NavBar";
-import LoginForm from "../components/LoginForm";
 
 const WelcomePage = () => {
     return (
         <div id="welcomePage">
-            <LoginForm/>
+            <NavBar>
+                <button>Sign Up</button>
+                <button>Login</button>
+            </NavBar>
+            <WelcomeHeader title="TMON" subtitle="Simple yet powerful tool for managing your sports team."/>
         </div>
     );
 };
