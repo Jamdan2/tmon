d4fdd73220a4acfa116348f368518a84458a8574
Aidan Heller <asfheller@gmail.com>
2017-10-31 21:56:24 -0700

    Style all forms

---
+++ src/app/js/components/LoginForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Form, Text} from 'react-form';
+import '../../scss/components/Form.css'
+
+const LoginForm = () => (
+    <div id="loginForm">
+        <Form>
+            { formApi => (
+                <form onSubmit={formApi.submitForm}>
+                    <div className="header">
+                        <h1>Login</h1>
+                    </div>
+                    <div className="content">
+                        <label>Username</label>
+                        <Text field="username"/>
+                    </div>
+                    <div className="content">
+                        <label>Password</label>
+                        <Text field="password" type="password"/>
+                    </div>
+                    <div className="content">
+                        <button type="submit">Submit</button>
+                        <button>Cancel</button>
+                    </div>
+                </form>
+            )}
+        </Form>
+    </div>
+);
+
+export default LoginForm;
