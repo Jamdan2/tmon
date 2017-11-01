37b3eb0d0fae27e9a12128f8090c5f635052b049
Aidan Heller <asfheller@gmail.com>
2017-10-31 21:29:20 -0700

    Make html skeleton of login form

--- src/app/js/pages/WelcomePage.js
+++ src/app/js/pages/WelcomePage.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import WelcomeHeader from "../components/WelcomeHeader";
 import NavBar from "../components/NavBar";
+import LoginForm from "../components/LoginForm";
 
 const WelcomePage = () => {
     return (
         <div id="welcomePage">
-            <NavBar>
-                <button>Sign Up</button>
-                <button>Login</button>
-            </NavBar>
-            <WelcomeHeader title="TMON" subtitle="Simple yet powerful tool for managing your sports team."/>
+            <div className="pop-ups">
+                <LoginForm/>
+            </div>
+            <div className="main">
+                <NavBar>
+                    <button>Sign Up</button>
+                    <button>Login</button>
+                </NavBar>
+                <WelcomeHeader title="TMON" subtitle="Simple yet powerful tool for managing your sports team."/>
+            </div>
         </div>
     );
 };
