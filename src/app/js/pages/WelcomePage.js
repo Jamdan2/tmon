38628a17b91e7f059df65871a15662f65279d71b
Aidan Heller <asfheller@gmail.com>
2017-11-11 22:49:33 -0800

    Undo bad code

--- src/app/js/pages/WelcomePage.js
+++ src/app/js/pages/WelcomePage.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import WelcomeHeader from "../components/WelcomeHeader";
 import NavBar from "../components/NavBar";
-import LoginForm from "../components/LoginForm";
 import '../../scss/pages/WelcomePage.css'
 
 const WelcomePage = () => {
     return (
         <div id="welcomePage">
-            <div className="pop-ups">
-                <LoginForm/>
-            </div>
-            <div className="main">
-                <NavBar>
-                    <button>Sign Up</button>
-                    <button>Login</button>
-                </NavBar>
-                <WelcomeHeader title="TMON" subtitle="Simple yet powerful tool for managing your sports team."/>
-            </div>
+            <NavBar>
+                <button>Sign Up</button>
+                <button>Login</button>
+            </NavBar>
+            <WelcomeHeader title="TMON" subtitle="Simple yet powerful tool for managing your sports team."/>
         </div>
     );
 };
